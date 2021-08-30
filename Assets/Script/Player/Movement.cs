816d1c4e45bde096ead693fd601ee00680428609
Fabricio Duarte <fabriciohod@hotmail.com>
2021-08-30 17:32:36 -0300

    Tirando movimentação na diagonal

--- Assets/Script/Player/Movement.cs
+++ Assets/Script/Player/Movement.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace YakisobaGang.Script.Player
@@ -8,6 +6,7 @@ namespace YakisobaGang.Script.Player
     public class Movement : MonoBehaviour
     {
         [SerializeField] private float speed = 100f;
+        [SerializeField] private bool disableDiagonalMovement = true;
         private PlayerInputActions _input;
         private Transform _myTransform;
         private Rigidbody _rigidbody;
@@ -33,8 +32,22 @@ namespace YakisobaGang.Script.Player
             _input.Gameplay.Move.performed -= MoveDirection;
         }
 
+        private void Update()
+        {
+            if(!disableDiagonalMovement)
+                return;
+            
+            // Enquanto o player tiver uma velocidade ele nao podera mudar de direcao
+            // assim ele nao irar se movimentar na diagonal
+            if(_rigidbody.velocity == Vector3.zero)
+                _input.Enable();
+            else
+                _input.Disable();
+        }
+
         private void MoveDirection(InputAction.CallbackContext ctx)
         {
+            // Adiciona um impulso na direcao que esta olhado
             _rigidbody.AddForce(_myTransform.forward * speed, ForceMode.Impulse);
         }
 
@@ -42,15 +55,19 @@ namespace YakisobaGang.Script.Player
         {
             Vector2 direction = ctx.ReadValue<Vector2>();
             
+            // Olhar para frente
             if(direction == Vector2.up)
                 _myTransform.rotation = Quaternion.identity;
-
+            
+            // Olhar para tras
             if (direction == Vector2.down)
                 _myTransform.rotation = Quaternion.Euler(0, 180, 0);
 
+            // Olhar para esquerda
             if (direction == Vector2.left)
                 _myTransform.rotation = Quaternion.Euler(0, -90, 0);
 
+            // Olhar para direita
             if (direction == Vector2.right)
                 _myTransform.rotation = Quaternion.Euler(0, 90, 0);
         }
