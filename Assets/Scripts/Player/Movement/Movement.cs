cb13a07bf54de66d251f5e1b6ee33ef44eb9bca1
Fabricio Duarte <fabriciohod@hotmail.com>
2021-09-14 16:02:50 -0300

    Refactoring no Movement.cs

--- Assets/Scripts/Player/Movement/Movement.cs
+++ Assets/Scripts/Player/Movement/Movement.cs
@@ -90,36 +90,4 @@ namespace YakisobaGang.Player.Movement
                 .SetEase(easeCurve);
         }
     }
-    
-    public class LookDirections
-    {
-        public Vector3 LookVector { get; private set; }
-        public Vector3 MoveDir { get; private set; }
-
-        public LookDirections() => LookForward();
-
-        public void LookForward()
-        {
-            LookVector = Vector3.zero;
-            MoveDir = Vector3.forward;
-        }
-
-        public void LookBackward()
-        {
-            LookVector = new Vector3(0, 180, 0);
-            MoveDir = Vector3.back;
-        }
-
-        public void LookLeft()
-        {
-            LookVector = new Vector3(0, -90, 0);
-            MoveDir = Vector3.left;
-        }
-
-        public void LookRight()
-        {
-            LookVector = new Vector3(0, 90, 0);
-            MoveDir = Vector3.right;
-        }
-    }
 }
