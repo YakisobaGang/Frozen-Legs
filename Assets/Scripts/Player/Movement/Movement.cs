3eb019ba7f6dc97f2d46d94722b8bf747da26d06
Fabricio Duarte <fabriciohod@hotmail.com>
2021-09-14 17:52:39 -0300

    _

--- Assets/Scripts/Player/Movement/Movement.cs
+++ Assets/Scripts/Player/Movement/Movement.cs
@@ -90,39 +90,4 @@ namespace YakisobaGang.Player.Movement
                 .SetEase(easeCurve);
         }
     }
-<<<<<<< HEAD
-=======
-
-    public class LookDirections
-    {
-        public Vector3 LookVector { get; private set; }
-        public Vector3 MoveDir { get; private set; }
-
-        public LookDirections() => LookForward();
-
-        public void LookForward()
-        {
-            LookVector = Vector3.zero;
-            MoveDir = Vector3.forward;
-        }
-
-        public void LookBackward()
-        {
-            LookVector = new Vector3(0, 180, 0);
-            MoveDir = Vector3.back;
-        }
-
-        public void LookLeft()
-        {
-            LookVector = new Vector3(0, -90, 0);
-            MoveDir = Vector3.left;
-        }
-
-        public void LookRight()
-        {
-            LookVector = new Vector3(0, 90, 0);
-            MoveDir = Vector3.right;
-        }
-    }
->>>>>>> b7bd544b693de6c8a0e39a58d9833b7daa6895e0
 }
