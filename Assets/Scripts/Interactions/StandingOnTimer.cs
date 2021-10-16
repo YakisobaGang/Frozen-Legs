7c74255e4429d03f5b348b41b3d267430c92ad29
Fabricio Duarte <fabriciohod@hotmail.com>
2021-10-16 17:09:38 -0300

    chão raxado e shader do gelo

---
+++ Assets/Scripts/Interactions/StandingOnTimer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace YakisobaGang.Interactions
+{
+    public class StandingOnTimer : MonoBehaviour
+    {
+        [SerializeField] private float time = 4f;
+        [SerializeField] private bool resetOnExit = false;
+        [HideInInspector] public float startTime;
+        public UnityEvent<float> onTimeTick;
+        public UnityEvent onTimeCompleted;
+
+
+        private void Awake()
+        {
+            startTime = time;
+        }
+
+        #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            var position = transform.position;
+            Handles.Label(new Vector3(position.x, 1, position.z), $"Timer: {time:N2}");
+        }
+        #endif
+
+        private void OnTriggerStay(Collider other)
+        {
+            if(!other.CompareTag("Player"))
+                return;
+            
+            time -= Time.deltaTime;
+            onTimeTick?.Invoke(time / startTime);
+
+            if (time <= 0f)
+            {
+                onTimeCompleted?.Invoke();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (resetOnExit)
+            {
+                time = startTime;
+            }
+        }
+    }
+}
