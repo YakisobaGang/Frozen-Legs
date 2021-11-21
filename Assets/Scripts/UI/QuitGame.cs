47393ab027c0f284aaeb6c80b6c00b729a930f80
Fabricio Duarte <fabriciohod@hotmail.com>
2021-11-21 18:05:37 -0300

    Menu principal e musica do level1

---
+++ Assets/Scripts/UI/QuitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace YakisobaGang.UI
+{
+    public class QuitGame : MonoBehaviour
+    {
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
