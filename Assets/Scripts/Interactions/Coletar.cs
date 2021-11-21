c8eae711e100a6c5e40078685ba7cebd049127a3
Silvano <silvanode.jesus@hotmail.com>
2021-11-21 20:11:59 -0300

    Adição do sistema de coletar ingredientes

--- Assets/Scripts/Interactions/Coletar.cs
+++ Assets/Scripts/Interactions/Coletar.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using YakisobaGang.Interactions;
 
 namespace YakisobaGang
 {
     public class Coletar : MonoBehaviour
     {
-        
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                Debug.Log("Pegou!");
+                ContadorDaColeta.adicionar();
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Interactions/ContadorDaColeta.cs
@@ -0,0 +1,29 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace YakisobaGang.Interactions
+{
+    public class ContadorDaColeta : MonoBehaviour
+    {
+        static int contador = 0;
+        public GameObject vitoria;
+        void Awake()
+        {
+            contador = 0;
+        }
+
+        void Update()
+        {
+            if (contador == 3)
+            {
+                vitoria.SetActive(true);
+                contador = 0;
+            }
+        }
+
+        public static void adicionar()
+        {
+            contador = contador + 1;
+        }
+    }
+}
